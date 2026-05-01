375a0e313493615817cd83ab193882c971b55791
Aryan Garg <gargaryan82000@gmail.com>
2026-05-01 17:00:29 +0530

    Learnt About Static, Dynamic Rendering, SSG and ISR in Next.js

--- my-first-app/app/(application)/blogs/[blogId]/page.tsx
+++ my-first-app/app/(application)/blogs/[blogId]/page.tsx
@@ -1,7 +1,21 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-export default async function Blog({ params }: { params: Promise<{ blogId: string }> }) {
+// By default it is true and the pages are not provided here, so that page gets generated as and when the user visits the page and that will be generated on fly.
+export const dynamicParams = false;
+
+export const revalidate = 10;
+
+// Generate Static Params
+export async function generateStaticParams() {
+  return [{ blogId: "1" }, { blogId: "2" }, { blogId: "3" }];
+}
+
+export default async function Blog({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
   // This params can be single level only nested and we cannot have /text after it.'
   const { blogId } = await params;
   if (!/^\d+$/.test(blogId)) {
--- my-first-app/app/(marketing)/services/page.tsx
+++ my-first-app/app/(marketing)/services/page.tsx
@@ -1,6 +1,16 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default function Services() {
+// This will make the page generated dynamically. This will render dynamically again and again if we come to this page again and again.
+export const dynamic = "force-dynamic";
+
+export default async function Services({ searchParams }) {
+  const search = await searchParams;
+  console.log(search);
+
+  const myCookies = await cookies();
+  console.log(myCookies);
+
   return (
     <>
       <h1>Welcome to services page</h1>
